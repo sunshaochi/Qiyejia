68be3816a7db631812a4e89a33aabf9f1165fb2c
王梦阳 <279809190@qq.com>
2016-12-29 13:31:53 +0800

    我的界面，修改资料，title布局

---
+++ app/src/main/java/com/boyuanitsm/echinfo/module/mine/ui/MineAct.java
@@ -0,0 +1,22 @@
+package com.boyuanitsm.echinfo.module.mine.ui;
+
+import android.os.Bundle;
+
+import com.boyuanitsm.echinfo.R;
+import com.boyuanitsm.echinfo.base.BaseAct;
+
+/**
+ * 我的界面
+ * Created by Yang on 2016/12/27 0027.
+ */
+public class MineAct extends BaseAct{
+    @Override
+    public int getLayout() {
+        return R.layout.act_mine;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
