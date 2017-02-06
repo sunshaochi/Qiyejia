42d71fb511658e977d3a8a0e9e0f1a43e25db1ce
王斌 <493204277@qq.com>
2017-02-06 10:33:39 +0800

    BaseRecyclerAdapter动画去掉

--- tools/src/main/java/com/boyuanitsm/tools/base/BaseRecyclerAdapter.java
+++ tools/src/main/java/com/boyuanitsm/tools/base/BaseRecyclerAdapter.java
@@ -5,10 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
-import com.boyuanitsm.tools.R;
 import com.boyuanitsm.tools.callback.OnItemClickListener;
 
 import java.util.ArrayList;
@@ -69,28 +66,28 @@ public abstract class BaseRecyclerAdapter<T> extends RecyclerView.Adapter<BaseRe
     @Override
     public void onBindViewHolder(BaseRecyclerViewHolder holder, int position) {
             bindData(holder, position, mData.get(position));
-            if (mUseAnimation) {
-                setAnimation(holder.itemView, position);
-            }
+//            if (mUseAnimation) {
+//                setAnimation(holder.itemView, position);
+//            }
     }
 
-    protected void setAnimation(View viewToAnimate, int position) {
-        if (position > mLastPosition) {
-            Animation animation = AnimationUtils
-                    .loadAnimation(viewToAnimate.getContext(), R.anim.item_bottom_in);
-            viewToAnimate.startAnimation(animation);
-            mLastPosition = position;
-        }
-    }
+//    protected void setAnimation(View viewToAnimate, int position) {
+//        if (position > mLastPosition) {
+//            Animation animation = AnimationUtils
+//                    .loadAnimation(viewToAnimate.getContext(), R.anim.item_bottom_in);
+//            viewToAnimate.startAnimation(animation);
+//            mLastPosition = position;
+//        }
+//    }
 
-    @Override
-    public void onViewDetachedFromWindow(BaseRecyclerViewHolder holder) {
-        super.onViewDetachedFromWindow(holder);
-        if (mUseAnimation && holder.itemView.getAnimation() != null && holder.itemView
-                .getAnimation().hasStarted()) {
-            holder.itemView.clearAnimation();
-        }
-    }
+//    @Override
+//    public void onViewDetachedFromWindow(BaseRecyclerViewHolder holder) {
+//        super.onViewDetachedFromWindow(holder);
+//        if (mUseAnimation && holder.itemView.getAnimation() != null && holder.itemView
+//                .getAnimation().hasStarted()) {
+//            holder.itemView.clearAnimation();
+//        }
+//    }
 
     public void add(int pos, T item) {
         mData.add(pos, item);
