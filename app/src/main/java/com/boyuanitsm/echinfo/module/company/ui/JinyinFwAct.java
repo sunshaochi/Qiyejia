294f74a4071cb6d9c652916e8b30b035e505949d
孙少池 <673132032@qq.com>
2017-02-08 19:43:02 +0800

    no message

--- app/src/main/java/com/boyuanitsm/echinfo/module/company/ui/JinyinFwAct.java
+++ app/src/main/java/com/boyuanitsm/echinfo/module/company/ui/JinyinFwAct.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 
 import com.boyuanitsm.echinfo.R;
 import com.boyuanitsm.echinfo.adapter.GvAdapter;
+import com.boyuanitsm.echinfo.adapter.TagAdapter;
 import com.boyuanitsm.echinfo.base.BaseAct;
 import com.boyuanitsm.echinfo.widget.MyGridView;
+import com.boyuanitsm.tools.view.FlowTag.FlowTagLayout;
+import com.boyuanitsm.tools.view.FlowTag.OnTagSelectListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -18,9 +24,13 @@ public class JinyinFwAct extends BaseAct {
     MyGridView gv_zczb;//注册资本
     @BindView(R.id.gv_clnx)
     MyGridView gv_clnx;//成立年限
-    @BindView(R.id.gv_ssfw)
-    MyGridView gv_ssfwb;//输入年限
+    @BindView(R.id.size_flow_layout)
+    FlowTagLayout mSizeFlowTagLayout;//
+
+    private TagAdapter<String> mSizeTagAdapter;
+
     private GvAdapter gvAdapter;
+
     @Override
     public int getLayout() {
         return R.layout.act_jinyinfw;
@@ -29,8 +39,37 @@ public class JinyinFwAct extends BaseAct {
     @Override
     public void init(Bundle savedInstanceState) {
         gvAdapter=new GvAdapter(JinyinFwAct.this);
-        gv_clnx.setAdapter(gvAdapter);
-        gv_zczb.setAdapter(gvAdapter);
+        gv_clnx.setAdapter(gvAdapter);//成立年限
+        gv_zczb.setAdapter(gvAdapter);//注册资本
+
+        mSizeTagAdapter = new TagAdapter<>(this);
+        mSizeFlowTagLayout.setTagCheckedMode(FlowTagLayout.FLOW_TAG_CHECKED_SINGLE);//设置是单选
+        mSizeFlowTagLayout.setAdapter(mSizeTagAdapter);
+        mSizeFlowTagLayout.setOnTagSelectListener(new OnTagSelectListener() {
+            @Override
+            public void onItemSelect(FlowTagLayout parent, List<Integer> selectedList) {
+
+            }
+        });
+
+        initSdata();//填充收索数据
 
     }
+
+
+    /**
+     * 填充数据
+     */
+    private void initSdata() {
+        List<String> sdatasource=new ArrayList<>();
+        sdatasource.add("按名称查");
+        sdatasource.add("按地址查");
+        sdatasource.add("按经营范围查");
+        sdatasource.add("按品牌/产品查");
+        sdatasource.add("按法定代表人查");
+        mSizeTagAdapter.onlyAddAll(sdatasource);
+    }
+
+
 }
+
---
+++ tools/src/main/java/com/boyuanitsm/tools/view/FlowTag/OnInitSelectedPosition.java
@@ -0,0 +1,9 @@
+package com.boyuanitsm.tools.view.FlowTag;
+
+/**
+ * Created by HanHailong on 16/6/26.
+ */
+
+public interface OnInitSelectedPosition {
+    boolean isSelectedPosition(int position);
+}
